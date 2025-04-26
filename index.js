35bd61b00c0b07924c483796b6b4c567717308df
aribamuskan <aribamuskan40@gmail.com>
2025-04-26 16:49:51 +0500

    Messagce

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: '*',   
+}));
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI )
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected!'))
   .catch(err => console.log(err));
 
@@ -16,4 +19,5 @@ mongoose.connect(process.env.MONGO_URI )
 const notesRoutes = require('./routes/notes');
 app.use('/api/notes', notesRoutes);
 
-app.listen(process.env.PORT, () => console.log('Server running on port 5000'));
+// 🚫 app.listen hata do
+module.exports = app;   // 👈 Yeh export karna hai Vercel ke liye
